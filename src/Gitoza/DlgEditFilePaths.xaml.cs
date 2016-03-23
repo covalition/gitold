a6c9cc61c5d51311278460e5223c76df213a4108
Roman Kowaliszyn <kowaliszyn@gazeta.pl>
2016-03-23 18:53:16 +0100

    New dialog - DlgEditFilePaths

--- src/Gitoza/DlgEditFilePaths.xaml.cs
+++ src/Gitoza/DlgEditFilePaths.xaml.cs
@@ -10,23 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Gitoza
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DlgEditFilePaths.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DlgEditFilePaths : Window
     {
-        public MainWindow() {
+        public DlgEditFilePaths() {
             InitializeComponent();
-        }
-
-        private void btnEditPathList_Click(object sender, RoutedEventArgs e) {
-            DlgEditFilePaths dlg = new DlgEditFilePaths();
-            dlg.ShowDialog();
         }
     }
 }
