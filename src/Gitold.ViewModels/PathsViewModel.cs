e683be169bfdc57194fc3452bf1631fc882163ea
Roman <kowaliszyn@gazeta.pl>
2016-06-02 19:25:55 +0200

    Delete and save

--- src/Gitold.ViewModels/PathsViewModel.cs
+++ src/Gitold.ViewModels/PathsViewModel.cs
@@ -17,7 +17,7 @@ namespace Gitold.ViewModels
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
-        protected override async Task<IEnumerable<PathViewModel>> GetItems() {
+        protected override async Task<IEnumerable<PathViewModel>> LoadItems() {
             return Properties.Settings.Default.LocalRepoPaths
                 .Cast<string>()
                 .Select(s => new PathViewModel
@@ -25,7 +25,25 @@ namespace Gitold.ViewModels
                     Caption = s
                 });
         }
-    }
 
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously     
+
+        protected override void Delete() {
+            Properties.Settings.Default.LocalRepoPaths.RemoveAt(SelectedItemIndex);
+            Properties.Settings.Default.Save();
+        }
+
+        protected override void Save() {
+            string path = (Fields[0] as FolderPathFieldViewModel).Value;
+            if (IsNew)
+                Properties.Settings.Default.LocalRepoPaths.Add(path);
+            else
+                Properties.Settings.Default.LocalRepoPaths[SelectedItemIndex] = path;
+            Properties.Settings.Default.Save();
+        }
+
+        protected override Task<List<LabeledFieldViewModel>> LoadFields() {
+            throw new NotImplementedException();
+        }
+    }
 }
